67ee503d47d4eff617f2ad5b7dc5a127f40601b9
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-11-30 18:46:16 +0300

    start

---
+++ src/main/java/com/skypro/arraylist/service/ArrayList.java
@@ -0,0 +1,5 @@
+package com.skypro.arraylist.service;
+
+public class ArrayList implements StringList{
+
+}
---
+++ src/main/java/com/skypro/arraylist/service/StringList.java
@@ -0,0 +1,5 @@
+package com.skypro.arraylist.service;
+
+public interface StringList {
+
+}
